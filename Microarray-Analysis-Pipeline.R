8d0a16cd50d0ff796f794aa69e79431a0db66871
Sam Thilmany <sam@thilmany.lu>
2022-10-21 14:08:29 +0200

    WIP

--- Microarray-Analysis-Pipeline.R
+++ Microarray-Analysis-Pipeline.R
@@ -4,7 +4,14 @@
 
 baseDir <- getwd()
 print(baseDir)
-targetsFile <- 'targets.tsv'
+
+graphicsDir <- paste0(baseDir, "/graphics")
+resultsDir <- paste0(baseDir, "/results")
+
+treatment = "GTx"
+experiment = 1
+
+targetsFile <- 'Targets.tsv'
 
 options(scipen = 99) # prevent scientific notation
 
@@ -26,13 +33,13 @@ require(tidyr)
 # ##################################
 
 # Create a folder for annotation files
-dir_annotation <- paste0(baseDir, "/annotation-files")
+annotationDir <- paste0(baseDir, "/annotation-files")
 
-if (!dir.exists(dir_annotation)) {
-  dir.create(dir_annotation)
+if (!dir.exists(annotationDir)) {
+  dir.create(annotationDir)
 }
 
-setwd(dir_annotation)
+setwd(annotationDir)
 
 tryCatch(
   expr = {
@@ -64,7 +71,8 @@ targetinfo <- readTargets(targetsFile, row.names = 'Name')
 targetinfo[order(targetinfo$Name),]
 
 # Select a subset for a specific experiment
-targetinfo = subset(targetinfo, Experiment == 1)
+targetinfo <- subset(targetinfo, Experiment == experiment)
+targetinfo <- subset(targetinfo, Treatment == treatment | Treatment == "NC")
 
 # Converts the raw data to an EListRaw object
 wtAgilent.GFilter <- function(qta) { qta[,"gIsPosAndSignif"] }
@@ -73,12 +81,10 @@ eset <- read.maimages(
   source = 'agilent.median',
   green.only = TRUE,
   path = "data",
+  names = targetinfo$Name,
   other.columns = 'gIsWellAboveBG',
   wt.fun = wtAgilent.GFilter
 )
-
-colnames(eset) <- row.names(targetinfo)
-eset <- eset[, order(colnames(eset))]
 
 # Add the spot type
 spotTypes <- readSpotTypes(file = 'SpotTypes.tsv')
@@ -91,7 +97,7 @@ eset$genes$Status <- controlStatus(spotTypes, eset)
 # ###################
 
 annotLookup <- read.csv(
-  paste0(dir_annotation, '/', annotationFile),
+  paste0(annotationDir, '/', annotationFile),
   header = TRUE,
   sep = '\t',
   stringsAsFactors = FALSE
@@ -117,7 +123,10 @@ eset$genes$external_gene_name <- annotLookup$external_gene_name
 # #############################
 
 eset <- backgroundCorrect(eset, method = 'normexp')
-plotDensities(eset, legend = FALSE, main = 'Density plot with background-corrected data')
+
+png(file = paste0(graphicsDir, '/', treatment, '_density-plot_bg-corrected.png'), width = 600, height = 350)
+plotDensities(eset, legend = FALSE, main = 'Density plot after background correction')
+dev.off()
 
 
 
@@ -126,15 +135,10 @@ plotDensities(eset, legend = FALSE, main = 'Density plot with background-correct
 # ##############
 
 eset <- normalizeBetweenArrays(eset, method = 'quantile')
-plotDensities(eset, legend = FALSE, main = 'Density plot with normalized, background-corrected data')
 
-
-
-# #######
-# MA-Plot
-# #######
-
-# plotMDS(eset)
+png(file = paste0(graphicsDir, '/', treatment, '_density-plot_normalized.png'), width = 600, height = 350)
+plotDensities(eset, legend = FALSE, main = 'Density plot after normalization')
+dev.off()
 
 
 
@@ -146,7 +150,7 @@ plotDensities(eset, legend = FALSE, main = 'Density plot with normalized, backgr
 
 Control <- eset$genes$ControlType != 0
 NoSymbol <- is.na(eset$genes$external_gene_name)
-IsExpr <- rowSums(eset$other$gIsWellAboveBG > 0) == 20
+IsExpr <- rowSums(eset$other$gIsWellAboveBG > 0) == length(colnames(eset))
 
 eset <- eset[!Control & !NoSymbol & IsExpr, ]
 
@@ -162,170 +166,23 @@ eset$genes <- eset$genes[,c(
 
 
 
-# #####################
-# Array quality weights
-# #####################
-
-array.weights <- arrayWeights(eset)
-barplot(array.weights, xlab = "Array", ylab = "Weight", main = "Array weights", col = "white", las = 2)
-abline(h = 1, lwd = 1, lty = 2)
-
-
-
 # #################
 # Expression values
 # #################
 
-boxplot(log2(eset$E), range = 0, ylab = "log2 intensity")
-
-
-# ###############################
-# Create a linear fit of the data
-# ###############################
-
-# array_batch <- factor(targetinfo$Array_Batch)
-# treatment <- factor(targetinfo$Treatment)
-# time <- factor(targetinfo$Time)
-# 
-# design <- model.matrix(~ array_batch + treatment + time)
-# 
-# fit <- lmFit(eset, design = design, weights = array.weights)
-# fit <- eBayes(fit)
+png(file = paste0(graphicsDir, '/', treatment, '_expression-values.png'), width = 600, height = 350)
+boxplot(log2(eset$E), main = "Expression values", ylab = "log2 intensity")
+dev.off()
 
 
 
-# ###########################
-# Create a Student's Q-Q Plot
-# ###########################
+# ############
+# Batch effect
+# ############
 
-# qqt(fit$t, df = fit$df.prior + fit$df.residual, pch = 16, cex = 0.2)
-# abline(0,1)
-
-
-
-# ################
-# Create a boxplot
-# ################
-
-# cols <- eset$targets$CellType
-# cols[cols == names$SH_SY5Y] <- "steelblue1"
-# cols[cols == names$SK_N_BE_2] <- "steelblue2"
-# cols[cols == names$Kelly] <- "steelblue3"
-# cols[cols == names$ReNcell_VM] <- "steelblue4"
-# cols[cols == names$Adult_Brain] <- "springgreen1"
-# cols[cols == names$Hippocampus] <- "springgreen2"
-# cols[cols == names$Insula] <- "springgreen3"
-# 
-# boxplot(eset$E ~ col(eset$E), names = rownames(eset$targets), col = cols, xlab = "Cell Type", ylab = "E-values", las = 2, mar = c(15, 4, 4, 2) + 0.1)
-
-
-
-# ##############################
-# Pairwise comparison of FC data
-# ##############################
-
-# S3 object containing the difference strings
-# differences <- list(Adult_Brain = '', Hippocampus = '', Insula = '')
-# class(differences) <- "Difference strings for FC comparison of different brain regions and cell lines"
-# 
-# differences$Adult_Brain <- list(SH_SY5Y = '', SK_N_BE_2 = '', Kelly = '', ReNcell_VM = '')
-# class(differences$Adult_Brain) <- "Difference strings for the adult brain"
-# 
-# differences$Hippocampus <- list(SH_SY5Y = '', SK_N_BE_2 = '', Kelly = '', ReNcell_VM = '')
-# class(differences$Hippocampus) <- "Difference strings for the hippocampus"
-# 
-# differences$Insula <- list(SH_SY5Y = '', SK_N_BE_2 = '', Kelly = '', ReNcell_VM = '')
-# class(differences$Insula) <- "Difference strings for the insula"
-
-# S3 object containing the contrast matrices
-# contrast_matrices <- list(SH_SY5Y = '', SK_N_BE_2 = '', Kelly = '', ReNcell_VM = '')
-# class(contrast_matrices) <- "Constrast matrices for FC comparison different brain regions and cell lines"
-
-# S3 object containing the fits
-# fits <- list(SH_SY5Y = '', SK_N_BE_2 = '', Kelly = '', ReNcell_VM = '')
-# class(fits) <- "Fits for FC comparison different brain regions and cell lines"
-
-# S3 object containing the results
-# results <- list(SH_SY5Y = '', SK_N_BE_2 = '', Kelly = '', ReNcell_VM = '')
-# class(results) <- "Results for FC comparison between different brain regions and cell lines"
-
-
-# Adult brain vs cell lines
-# differences$Adult_Brain$SH_SY5Y <- paste0(names$Adult_Brain,"-",names$SH_SY5Y)
-# differences$Adult_Brain$SK_N_BE_2 <- paste0(names$Adult_Brain,"-",names$SK_N_BE_2)
-# differences$Adult_Brain$Kelly <- paste0(names$Adult_Brain,"-",names$Kelly)
-# differences$Adult_Brain$ReNcell_VM <- paste0(names$Adult_Brain,"-",names$ReNcell_VM)
-
-# Hippocampus vs cell lines
-# differences$Hippocampus$SH_SY5Y <- paste0(names$Hippocampus,"-",names$SH_SY5Y)
-# differences$Hippocampus$SK_N_BE_2 <- paste0(names$Hippocampus,"-",names$SK_N_BE_2)
-# differences$Hippocampus$Kelly <- paste0(names$Hippocampus,"-",names$Kelly)
-# differences$Hippocampus$ReNcell_VM <- paste0(names$Hippocampus,"-",names$ReNcell_VM)
-
-# Insula vs cell lines
-# differences$Insula$SH_SY5Y <- paste0(names$Insula,"-",names$SH_SY5Y)
-# differences$Insula$SK_N_BE_2 <- paste0(names$Insula,"-",names$SK_N_BE_2)
-# differences$Insula$Kelly <- paste0(names$Insula,"-",names$Kelly)
-# differences$Insula$ReNcell_VM <- paste0(names$Insula,"-",names$ReNcell_VM)
-
-
-# Tests for the SH-SY5Y cell line
-# contrast_matrices$SH_SY5Y <- makeContrasts(
-#   differences$Adult_Brain$SH_SY5Y,
-#   differences$Hippocampus$SH_SY5Y,
-#   differences$Insula$SH_SY5Y,
-#   levels = design
-# )
-# 
-# fits$SH_SY5Y <- contrasts.fit(fit, contrast_matrices$SH_SY5Y)
-# fits$SH_SY5Y <- eBayes(fits$SH_SY5Y, trend = TRUE, robust = TRUE)
-# results$SH_SY5Y <- decideTests(fits$SH_SY5Y, method = "separate", adjust.method = "BH", p.value = 0.01, lfc = 2)
-
-# Tests for the SK-N-BE(2) cell line
-# contrast_matrices$SK_N_BE_2 <- makeContrasts(
-#   differences$Adult_Brain$SK_N_BE_2,
-#   differences$Hippocampus$SK_N_BE_2,
-#   differences$Insula$SK_N_BE_2,
-#   levels = design
-# )
-# 
-# fits$SK_N_BE_2 <- contrasts.fit(fit, contrast_matrices$SK_N_BE_2)
-# fits$SK_N_BE_2 <- eBayes(fits$SK_N_BE_2, trend = TRUE, robust = TRUE)
-# results$SK_N_BE_2 <- decideTests(fits$SK_N_BE_2, method = "separate", adjust.method = "BH", p.value = 0.01, lfc = 2)
-
-# Tests for the Kelly cell line
-# contrast_matrices$Kelly <- makeContrasts(
-#   differences$Adult_Brain$Kelly,
-#   differences$Hippocampus$Kelly,
-#   differences$Insula$Kelly,
-#   levels = design
-# )
-# 
-# fits$Kelly <- contrasts.fit(fit, contrast_matrices$Kelly)
-# fits$Kelly <- eBayes(fits$Kelly, trend = TRUE, robust = TRUE)
-# results$Kelly <- decideTests(fits$Kelly, method = "separate", adjust.method = "BH", p.value = 0.01, lfc = 2)
-
-# Tests for the ReNcell VM cell line
-# contrast_matrices$ReNcell_VM <- makeContrasts(
-#   differences$Adult_Brain$ReNcell_VM,
-#   differences$Hippocampus$ReNcell_VM,
-#   differences$Insula$ReNcell_VM,
-#   levels = design
-# )
-# 
-# fits$ReNcell_VM <- contrasts.fit(fit, contrast_matrices$ReNcell_VM)
-# fits$ReNcell_VM <- eBayes(fits$ReNcell_VM, trend = TRUE, robust = TRUE)
-# results$ReNcell_VM <- decideTests(fits$ReNcell_VM, method = "separate", adjust.method = "BH", p.value = 0.01, lfc = 2)
-
-
-
-# ############################################
-# Replace replicate arrays with the mean value
-# ############################################
-
-# eset_ave <- avearrays(eset, ID = eset$targets$CellType, weights = eset$weights)
-# eset_ave <- eset_ave[rowSums(is.na(eset_ave$E)) == 0,]
-# colnames(eset_ave$E) <- c('Adult Brain', 'Hippocampus', 'Insula', 'Kelly', 'ReNcell VM', 'SH-SY5Y', 'SK-N-BE(2)')
+png(file = paste0(graphicsDir, '/', treatment, '_batch-effect.png'), width = 600, height = 350)
+plotMDS(eset, labels = eset$targets$Array_Batch)
+dev.off()
 
 
 
@@ -333,15 +190,106 @@ boxplot(log2(eset$E), range = 0, ylab = "log2 intensity")
 # Create a heatmap
 # ################
 
-# coolmap(eset_ave, cluster.by="de pattern", show.dendrogram = "column", margins = c(7, 1), srtCol=45, labRow='')
-# 
-# coolmap(eset_ave, cluster.by="expression level", show.dendrogram = "column", col = "redblue", margins = c(7, 1), srtCol=45, labRow='')
+png(file = paste0(graphicsDir, '/', treatment, '_de-pattern.png'), width = 600, height = 350)
+coolmap(eset$E, cluster.by = "de pattern", show.dendrogram = "column", margins = c(7, 1), srtCol = 45, labRow = '', main = "Clustered by DE")
+dev.off()
+
+png(file = paste0(graphicsDir, '/', treatment, '_expression-levels.png'), width = 600, height = 350)
+coolmap(eset$E, cluster.by="expression level", show.dendrogram = "column", col = "redblue", margins = c(7, 1), srtCol=45, labRow='', main = "Clustered by EL")
+dev.off()
 
 
 
-# #################
-# Create a PCA plot
-# #################
+# #####################
+# Array quality weights
+# #####################
 
-# plotMDS(eset_ave, labels = NULL, pch = c(rep(15, 3), rep(16, 4)), col = 1:7)
-# legend(-3.5, 4, legend = colnames(eset_ave$E), col = 1:7, pch = c(rep(15, 3), rep(16, 4)), bty = 'n', cex = 0.9)
+array.weights <- arrayWeights(eset)
+
+png(file = paste0(graphicsDir, '/', treatment, '_array-weights.png'), width = 600, height = 350)
+barplot(array.weights, xlab = "Array", ylab = "Weight", main = "Array weights", col = "white", las = 2)
+abline(h = 1, lwd = 1, lty = 2)
+dev.off()
+
+
+
+# ###############################
+# Create a linear fit of the data
+# ###############################
+
+time_factor <- factor(targetinfo$Time)
+
+design <- model.matrix(~ 0 + time_factor)
+
+fit <- lmFit(eset, design = design, weights = array.weights)
+
+
+
+# ##############
+# Make contrasts
+# ##############
+
+contrasts <- makeContrasts(
+  time_factor2-time_factor0,
+  time_factor5-time_factor0,
+  time_factor7-time_factor0,
+  time_factor10-time_factor0,
+  levels = design
+)
+fit <- contrasts.fit(fit, contrasts)
+fit <- eBayes(fit)
+
+
+
+# #############
+# Venn diagrams
+# #############
+
+png(file = paste0(graphicsDir, '/', treatment, '_vennDiagram.png'), width = 600, height = 350)
+vennDiagram(fit, include = c("up", "down"), names = c("D2-D0", "D5-D0", "D7-D0", "D10-D0"), counts.col = c("red", "blue"), main = "Up- and down-regulated genes")
+dev.off()
+
+
+
+# ############
+# Save results
+# ############
+
+# Create a folder for result files
+if (!dir.exists(resultsDir)) {
+  dir.create(resultsDir)
+}
+
+treatment_deg_D2 <- topTable(fit, coef = 1, n = Inf)
+treatment_deg_D5 <- topTable(fit, coef = 2, n = Inf)
+treatment_deg_D7 <- topTable(fit, coef = 3, n = Inf)
+treatment_deg_D10 <- topTable(fit, coef = 4, n = Inf)
+
+treatment_deg_D2_top20 <- topTable(fit, coef = 1, n = 20)
+treatment_deg_D5_top20 <- topTable(fit, coef = 2, n = 20)
+treatment_deg_D7_top20 <- topTable(fit, coef = 3, n = 20)
+treatment_deg_D10_top20 <- topTable(fit, coef = 4, n = 20)
+
+setwd(resultsDir)
+
+tryCatch(
+  expr = {
+    write.csv(treatment_deg_D2, file = paste0("treatment", treatment, "_deg_D2.csv"), row.names = FALSE)
+    write.csv(treatment_deg_D5, file = paste0("treatment", treatment, "_deg_D5.csv"), row.names = FALSE)
+    write.csv(treatment_deg_D7, file = paste0("treatment", treatment, "_deg_D7.csv"), row.names = FALSE)
+    write.csv(treatment_deg_D10, file = paste0("treatment", treatment, "_deg_D10.csv"), row.names = FALSE)
+    
+    write.csv(treatment_deg_D2_top20, file = paste0("treatment", treatment, "_deg_D2_top20.csv"), row.names = FALSE)
+    write.csv(treatment_deg_D5_top20, file = paste0("treatment", treatment, "_deg_D5_top20.csv"), row.names = FALSE)
+    write.csv(treatment_deg_D7_top20, file = paste0("treatment", treatment, "_deg_D7_top20.csv"), row.names = FALSE)
+    write.csv(treatment_deg_D10_top20, file = paste0("treatment", treatment, "_deg_D10_top20.csv"), row.names = FALSE)
+  },
+  finally = {
+    setwd(baseDir)
+  }
+)
+
+png(file = paste0(graphicsDir, '/', treatment, '_volcanoplot.png'), width = 600, height = 350)
+volcanoplot(fit, coef = 1)
+dev.off()
+
